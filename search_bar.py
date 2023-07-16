85ff8596492dd3119f36f65ba8ddac470da5d403
Laguting <lagutingmariconjaneg@gmail.com>
2023-07-16 20:44:19 +0800

    Place the display for the searched information

--- search_bar.py
+++ search_bar.py
@@ -25,14 +25,28 @@ class Search_Bar(tk.Frame):
         self.bg_name.configure(image=self.bg_image)
 
 # Search Entry
-        self.search_label = tk.Label(self, text="Search by Name:")
+        # Search bar label
+        self.search_label = tk.Label(self, bg="yellow", fg="black",text="Search by Name:")
         self.search_label.place(x=30, y=45)
-
+        # Search bar entry
         self.search_entry = tk.Entry(self, width=100)
         self.search_entry.place(x=150, y=45)
-
+        # Search button
         self.search_button = tk.Button(self, text="Search", command=lambda: self.change_section(0)) # temporary so that the search button will appear
         self.search_button.place(x=780, y=45)
+        # Search Result
+        self.result_label = tk.Label(self, text="") # Display the result of the searched informaion
+        self.result_label.place(x=30, y=100)
+    
+       
+# Navigation buttons
+        # Back button
+        self.back_button = tk.Button(self, text="Back to Home", command=lambda: self.change_section(0))
+        self.back_button.place(x=250, y=720)
+
+        # Exit button
+        self.exit_button = tk.Button(self, text="Exit", command=lambda: self.change_section(2))
+        self.exit_button.place(x=650, y=720)
 
     def present(self):
         self.pack()
