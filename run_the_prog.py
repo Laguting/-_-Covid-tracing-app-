9b118986aa80f5f296c5cd6443d2a80b80537d6e
Laguting <lagutingmariconjaneg@gmail.com>
2023-07-16 19:53:59 +0800

    Fixed the back to home at exit button display because before it presents half of the previous section and the suppose section it should display

--- run_the_prog.py
+++ run_the_prog.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from home import Home
 from registration import Register
+from reminders import Reminders
 
 class CovidTrace(tk.Tk):
     def __init__(self):
@@ -15,7 +16,8 @@ class CovidTrace(tk.Tk):
         # Sections
         self.section_0 = Home(self, self.change_section)
         self.section_1 = Register(self, self.change_section)
-        self.sections = [self.section_0, self.section_1]
+        self.section_2 = Reminders(self, self.change_section)
+        self.sections = [self.section_0, self.section_1, self.section_2]
         
         # Show initial section
         self.present_section(self.currentsection)
@@ -26,7 +28,8 @@ class CovidTrace(tk.Tk):
         self.present = section_n
     
     def change_section(self, section_n):
-        self.sections[self.currentsection].hide()
+        for section in self.sections:
+            section.hide()
         self.present_section(section_n)
 
 if __name__ == "__main__":
