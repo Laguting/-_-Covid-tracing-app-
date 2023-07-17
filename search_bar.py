bc02f049c2c356a7f857f079ae9f2464ed2b2c2c
Laguting <lagutingmariconjaneg@gmail.com>
2023-07-18 04:18:36 +0800

    Modified the display of regitration info to make it readable

--- search_bar.py
+++ search_bar.py
@@ -15,54 +15,58 @@ class Search_Bar(tk.Frame):
         self.bg = ImageTk.PhotoImage(self.initial_bg)
         self.bg_name = tk.Label(self, image=self.bg)
         self.bg_name.place(x=0, y=0, relwidth=1, relheight=1)
-        self.bind("<Configure>", self.resize_image) 
-       
-        # Resize image
+        self.bind("<Configure>", self.resize_image)
+
+# Search
+    # Search label
+        self.search_label = tk.Label(self, bg="yellow", fg="black", text="Search by Name:")
+        self.search_label.place(x=30, y=45)
+    # Search entry
+        self.search_entry = tk.Entry(self, width=100)
+        self.search_entry.place(x=250, y=45)
+    # Search button
+        self.search_button = tk.Button(self, text="Search", command=self.search_registration)
+        self.search_button.place(x=780, y=45)
+    # Search button result
+        self.result_label = tk.Label(self, bg="yellow", fg="black", text="")
+        self.result_label.place(x=150, y=100)
+    # Present Search
+        self.present_button = tk.Button(self, text="View Entry", command=self.view_entry, state=tk.DISABLED)
+        self.present_button.place(x=450, y=720)
+
+        self.registration_info = ""
+
+# Navigation buttons
+    # Back to home
+        self.back_button = tk.Button(self, text="Back to Home", command=lambda: self.change_section(0))
+        self.back_button.place(x=250, y=720)
+    # Exit
+        self.exit_button = tk.Button(self, text="Exit", command=lambda: self.change_section(2))
+        self.exit_button.place(x=650, y=720)
+
+# Background image
     def resize_image(self, event):
         new_width = event.width
         new_height = event.height
         resized_image = self.initial_bg.resize((new_width, new_height), Image.NEAREST)
         self.bg_image = ImageTk.PhotoImage(resized_image)
         self.bg_name.configure(image=self.bg_image)
-
-# Search Entry
-        # Search bar label
-        self.search_label = tk.Label(self, bg="yellow", fg="black",text="Search by Name:")
-        self.search_label.place(x=30, y=45)
-        # Search bar entry
-        self.search_entry = tk.Entry(self, width=100)
-        self.search_entry.place(x=150, y=45)
-        # Search button
-        self.search_button = tk.Button(self, text="Search", command=self.search_registration) # Search the entry
-        self.search_button.place(x=780, y=45)
-        # Search Result
-        self.result_label = tk.Label(self, bg="yellow", fg="black" ,text="") # Display the result of the searched informaion
-        self.result_label.place(x=200, y=100)
-
-# Navigation buttons
-        # Back button
-        self.back_button = tk.Button(self, text="Back to Home", command=lambda: self.change_section(0))
-        self.back_button.place(x=250, y=720)
-
-        # Exit button
-        self.exit_button = tk.Button(self, text="Exit", command=lambda: self.change_section(2))
-        self.exit_button.place(x=650, y=720)
-
-# Search entry confirmation
+        
+# Search Registration information
     def search_registration(self):
         name_to_search = self.search_entry.get()
         registration_info = self.get_registration_info(name_to_search)
 
         if registration_info:
-            self.result_label.config(text=registration_info)
+            self.registration_info = registration_info
+            self.result_label.config(text="Registration found!")
             self.present_button.config(state=tk.NORMAL)
             messagebox.showinfo("Search Result", "Registration found!")
         else:
             self.result_label.config(text="No matching registration found.")
             self.present_button.config(state=tk.DISABLED)
             messagebox.showinfo("Search Result", "No matching registration found.")
-            
-# Get the registration information
+# Get registration information
     def get_registration_info(self, name):
         with open("user_inputs_folder.txt", "r") as file:
             lines = file.readlines()
@@ -79,13 +83,31 @@ class Search_Bar(tk.Frame):
                 registration_info += line + "\n"
             elif is_matching_registration:
                 registration_info += line + "\n"
-                if line.startswith("Name: "):
+                if line.startswith("Close Contact Email Address:"):
                     is_matching_registration = False
         return registration_info if found else ""
-        
+
+# Display information modification
+    def display_registration_info(self):
+        self.result_label.config(text="")
+        registration_lines = self.registration_info.split("\n")
+        for line in registration_lines:
+            label, value = line.split(":", 1)
+            label = label.strip()
+            value = value.strip()
+            label_label = tk.Label(self, text=label, bg="yellow", fg="black")
+            label_label.pack(anchor=tk.W, padx=30)
+            value_label = tk.Label(self, text=value, bg="yellow", fg="black")
+            value_label.pack(anchor=tk.W, padx=30) 
+
+# Ask the user first if they wanted to view their entry
+    def view_entry(self):
+        confirmation = messagebox.askyesno("View Entry", "Do you want to view the registration entry?")
+        if confirmation:
+            self.result_label.config(text=self.registration_info)
+
     def present(self):
         self.pack()
 
     def hide(self):
         self.pack_forget()
-    
